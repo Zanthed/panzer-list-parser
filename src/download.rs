0e1d5b907e62b8725ab3560b5ddc3bf12250c937
Loron groebner <raizo@riseup.net>
2021-04-06 02:27:00 +0200

    ✨ Downloader finished

--- src/download.rs
+++ src/download.rs
@@ -1,5 +1,6 @@
-use std::{fs::{File, read_dir, read_to_string, remove_dir, remove_file}, io::{self, Write}, path::Path, process::exit};
+use std::{any::Any, fs::{File, read_dir, read_to_string, remove_dir, remove_file}, io::{self, Write}, path::Path, process::exit};
 use serde::{Serialize, Deserialize};
+use serde_json::{Value, from_str};
 use zip::ZipArchive;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -10,47 +11,56 @@ pub struct FileInfo {
     update_url: String
 }
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct List {
+pub struct DownloadList {
     #[serde(alias = "$schema")]
     schema: String,
     file_info: FileInfo
 }
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Playerarray {
+    steamid: Value,
+    attributes: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct List {
+    #[serde(alias = "$schema")]
+    schema: String,
+    file_info: FileInfo,
+    players: Vec<Playerarray>
+}
 
 
 pub struct Downloader { 
-    client: reqwest::Client,
-    lists: Vec<List>
+    client: reqwest::Client
 }
 
 /* supposed to download/unpack and parse the lists */
 impl Downloader {
     pub fn new() -> Self {
         Self {
-            client: reqwest::Client::new(),
-            lists: vec![],
+            client: reqwest::Client::new()
         }
     }
 
 
     /* downloads the zip and writes it to a .zip via rust I/O */
-    pub async fn download_lists(&mut self, url: String) -> Result<(), reqwest::Error> {
+    pub async fn download_lists(&mut self, url: String) -> Result<(), Box<dyn std::error::Error>> {
 
         let resp = self.client.get(url).send().await?;
         let bytes = resp.bytes().await?;
 
-        if Path::new("lists.zip").is_dir() {
-            remove_file("lists.zip").unwrap();
-        }
-
-        let mut file = File::create("lists/lists.zip").unwrap();
+        let mut file = File::create("lists/misc/lists.zip").unwrap();
         file.write_all(&bytes).unwrap();
+
+        Downloader::unzip(self).await?;
 
         Ok(())
     }
 
     /* unzips the file using the zip crate */
-    pub async fn unzip(&mut self) -> Result<(), std::io::Error> {
-        let file = File::open("lists/lists.zip").unwrap();
+    pub async fn unzip(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let file = File::open("lists/misc/lists.zip").unwrap();
         let mut archive = ZipArchive::new(file).unwrap();
 
         /* iterate over all fils in archive */
@@ -58,7 +68,7 @@ impl Downloader {
             let mut file = archive.by_index(i).unwrap();
 
             let path = match file.enclosed_name() {
-                Some(path ) => "lists/".to_owned() + path.to_str().unwrap(),
+                Some(path ) => "lists/misc/".to_owned() + path.to_str().unwrap(),
                 None => exit(-1),
             };
 
@@ -67,23 +77,40 @@ impl Downloader {
         }
         
         /* delete zip again */
-        remove_file("lists/lists.zip").unwrap();
+        remove_file("lists/misc/lists.zip").unwrap();
+
+        Downloader::parse_lists(self);
+
         Ok(())
     }
 
     /* parses json into custom struct */
-    pub async fn parse_lists(&mut self) -> Result<(), serde_json::Error> {
-        for file in read_dir("lists").unwrap() {
+    pub async fn parse_lists(&mut self) -> Result<Vec<List>, Box<dyn std::error::Error>> {
+        let mut lists: Vec<List> = vec![];
+        for file in read_dir("lists/misc/").unwrap() {
             let file = file.unwrap();
-            let content = read_to_string("lists/".to_owned() + file.file_name().to_str().unwrap()).unwrap();
+            let content = read_to_string("lists/misc/".to_owned() + file.file_name().to_str().unwrap()).unwrap();
 
-            let list: List = serde_json::from_str(&content).unwrap();
+            let dlist: DownloadList = serde_json::from_str(&content).unwrap();
 
-            self.lists.push(list);
+            /* download actual list */
+            let resp = self.client.get(dlist.file_info.update_url).send().await?;
+            let bytes = &resp.bytes().await?;
+
+            let fname = "lists/".to_owned() + &dlist.file_info.title + ".json";
+            
+            if fname.contains("Moeb") {
+                continue;
+            }
+
+            let mut file = File::create(&fname).unwrap();
+            file.write_all(&bytes).unwrap();   
+            let filecontent = read_to_string(&fname).unwrap();
+            let list: List = serde_json::from_str(&filecontent).unwrap();
+
+            lists.push(list);
         }
-
-        println!("{:?}", self.lists);
-
-        Ok(())
+        Ok(lists)
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::create_dir, path::Path, process::exit};
+use std::{fs::{create_dir, create_dir_all}, path::Path, process::exit};
 
 mod download;
 mod handler;
@@ -8,17 +8,14 @@ use handler::Handler;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if !Path::new("lists").is_dir() {
-        create_dir("lists").expect("Unable to create lists directory");
+    if !Path::new("lists/misc").is_dir() {
+        create_dir_all("lists/misc").expect("Unable to create lists directory");
         exit(-1);
     }
 
     let mut downloader = Downloader::new();
-
-    
+ 
     downloader.download_lists("https://github.com/incontestableness/TF2BD-Community-Lists/raw/main/All.zip".to_string()).await?;
-    downloader.unzip().await?;
-    downloader.parse_lists().await?;
     
 
     Ok(())
